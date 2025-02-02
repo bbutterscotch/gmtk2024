bc1de781cfec8f2f9a73f25c4cc43dc5ff3592ce
Emily <47927178+bbutterscotch@users.noreply.github.com>
2025-02-01 23:40:42 -0500

    Build Settings

--- gmtk2024/Assets/Scripts/Bee.cs
+++ gmtk2024/Assets/Scripts/Bee.cs
@@ -1,11 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-using static UnityEditor.FilePathAttribute;
 using FMODUnity;
-using UnityEditor.MemoryProfiler;
 
 public class Bee : MonoBehaviour
 {
